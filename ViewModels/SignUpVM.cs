b3487247809c83c32d3d3f7727fed64dfb8a84fb
an <108365444+amxrac@users.noreply.github.com>
2025-02-01 18:01:34 +0100

    add login and signup view models

---
+++ ViewModels/SignUpVM.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace rgproj.ViewModels
+{
+    public class SignUpVM
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords don't match.")]
+        [Display(Name = "Confirm Password")]
+
+        public string? ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+
+        public string? Location { get; set; }
+
+        [Required]
+        public string? Role { get; set; }
+    }
+}
